03db16cda44b46b65921427701fd73de483fb486
atul <atul@tablecheck.com>
2021-12-31 15:40:30 +0900

    Day 2 Complete

--- Day 02/main.py
+++ Day 02/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# Path: Day 02/main.py
 
 def answer():
     X = 0
@@ -16,4 +16,22 @@ def answer():
     return X*Y
 
 
-print(answer())
+def answer_pt2():
+    X = 0
+    Y = 0
+    aim = 0
+    with open("input.txt", "r") as f:
+        for inst in f.read().strip().split("\n"):
+            [direc, mag] = inst.split()
+            if direc == "forward":
+                X += int(mag)
+                Y += aim*int(mag)
+            elif direc == "down":
+                aim += int(mag)
+            elif direc == "up":
+                aim += -int(mag)
+    return X*Y
+
+if __name__ == "__main__":
+    print(answer())
+    print(answer_pt2())
